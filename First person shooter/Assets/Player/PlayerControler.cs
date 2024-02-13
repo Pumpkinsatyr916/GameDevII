516a2f394fa6b0aae453f74d515e1b3b03e780d5
Pumpkinsatyr916 <Dl272721@ohio.edu>
2024-02-13 10:38:10 -0500

    started state machine

--- First person shooter/Assets/Player/PlayerControler.cs
+++ First person shooter/Assets/Player/PlayerControler.cs
@@ -7,7 +7,7 @@ using TMPro;
 public class PlayerControler : MonoBehaviour
 {
     //Debug
-    public TMP_Text debug_text;
+    //public TMP_Text debug_text;
     
     //Camera Variables
     public Camera cam;
@@ -21,7 +21,7 @@ public class PlayerControler : MonoBehaviour
     [Range(0.01f, 1f)]public float sensitivity;
 
     // player Input
-    private Vector2 move_input;
+    /*private Vector2 move_input;
     private bool grounded; 
 
     //movement 
@@ -33,7 +33,7 @@ public class PlayerControler : MonoBehaviour
     public float gravity = 15f;
     public float stop_speed = 0.5f;
     public float jump_impulse = 10f;
-    public float friction = 4f;
+    public float friction = 4f;*/
 
     // Start is called before the first frame update
     void Start()
@@ -47,23 +47,23 @@ public class PlayerControler : MonoBehaviour
         if (invert_y) invert_factor_y = -1;
 
         //get ref to character controller compented 
-        characterController = GetComponent<CharacterController>();
+        //characterController = GetComponent<CharacterController>();
     }
 
 
     // Update is called once per frame
     void Update()
     {
-        debug_text.text = "Wish Dir: " + wish_dir.ToString();
+       /* debug_text.text = "Wish Dir: " + wish_dir.ToString();
         debug_text.text += "\nVelocity: " + player_velocity.ToString();
         debug_text.text += "\nSpeed: " + new Vector3(player_velocity.x, 0, player_velocity.z).magnitude.ToString();
-        debug_text.text += "\nGround: " + grounded.ToString();
+        debug_text.text += "\nGround: " + grounded.ToString();*/
 
         Look();
 
     }
 
-    private void FixedUpdate()
+    /*private void FixedUpdate()
     {
         // Find wish_dir.
         wish_dir = transform.right * move_input.x + transform.forward * move_input.y;
@@ -87,7 +87,7 @@ public class PlayerControler : MonoBehaviour
         }
 
         characterController.Move(player_velocity * Time.deltaTime);
-    }
+    }*/
 
     public void GetLookInput(InputAction.CallbackContext context)
     {
@@ -106,25 +106,25 @@ public class PlayerControler : MonoBehaviour
         horizontal_look_angle = Mathf.Clamp(horizontal_look_angle, -90, 90);
         cam.transform.localRotation = Quaternion.Euler(horizontal_look_angle, 0, 0);
     }
-    public void GetMoveInput(InputAction.CallbackContext context)
+    /*public void GetMoveInput(InputAction.CallbackContext context)
     {
         move_input = context.ReadValue<Vector2>();
 
     }
-    public void GetJumpInput(InputAction.CallbackContext context)
+   /* public void GetJumpInput(InputAction.CallbackContext context)
     {
         Jump();
 
-    }
-    public void Jump()
+    }*/
+    /*public void Jump()
     {
         if (grounded)
         {
             player_velocity.y = jump_impulse;
         }
-    }
+    }*/
 
-    private Vector3 Accelerate(Vector3 wish_dir, Vector3 current_velocity, float  accel, float mad_speed)
+  /*  private Vector3 Accelerate(Vector3 wish_dir, Vector3 current_velocity, float  accel, float mad_speed)
     {
         float proj_speed = Vector3.Dot(current_velocity, wish_dir);
         float accel_speed = accel * Time.deltaTime;
@@ -161,5 +161,5 @@ public class PlayerControler : MonoBehaviour
         new_velocity = new Vector3(new_velocity.x, current_velocity.y, new_velocity.z);
 
         return Accelerate(wish_dir, new_velocity, acceleration, max_speed);
-    }
+    }*/
 }
---
+++ First person shooter/Assets/Player/State Machine/PlayerGroundState.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using UnityEngine;
+
+public class PlayerGroundState : PlayerBaseState
+{
+    public override void EnterState(PlayerStateMachine state_machine)
+    {
+        
+    }
+    public override void ExitState(PlayerStateMachine state_machine)
+    {
+        
+    }
+    public override void UpdateState(PlayerStateMachine state_machine)
+    {
+        
+    }
+    public override void FixedUpdateState(PlayerStateMachine state_machine)
+    {
+
+    }
+}
